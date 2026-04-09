5de4f82d6acb87855c03925bfbecf76c300ce129
bailliemaweu-maker <bailliemaweu@gmail.com>
2026-04-09 12:52:49 +0300

    Secondpush

--- app/src/main/java/com/example/bailliemaweu/MainActivity.kt
+++ app/src/main/java/com/example/bailliemaweu/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.bailliemaweu
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,44 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+//        fetch 8 buttons by their Ids
+        val recipebutton = findViewById<Button>(R.id.recipe)
+        val advicebutton = findViewById<Button>(R.id.advice)
+        val meditationbutton = findViewById<Button>(R.id.meditation)
+        val hydrationbutton = findViewById<Button>(R.id.hydration)
+        val exercisebutton = findViewById<Button>(R.id.exercise)
+        val goalsbutton = findViewById<Button>(R.id.goals)
+        val progressbutton = findViewById<Button>(R.id.progress)
+
+        recipebutton.setOnClickListener {
+            val intent = Intent(applicationContext, HealthActivity::class.java)
+            startActivity(intent)
+        }
+//        ==============================================================================
+        advicebutton.setOnClickListener {
+            val intent = Intent(applicationContext, NutritionActivity::class.java)
+            startActivity(intent)
+        }
+        meditationbutton.setOnClickListener {
+            val intent = Intent( applicationContext, MeditationActivity::class.java )
+        startActivity(intent)
+        }
+        hydrationbutton.setOnClickListener {
+            val intent = Intent(applicationContext, HydrationActivity::class.java)
+            startActivity(intent)
+        }
+        exercisebutton.setOnClickListener {
+            val intent = Intent(applicationContext, ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+        goalsbutton.setOnClickListener {
+            val intent = Intent(applicationContext, WeeklyActivity::class.java)
+            startActivity(intent)
+        }
+        progressbutton.setOnClickListener {
+            val intent = Intent(applicationContext, ProgressActivity::class.java)
+            startActivity(intent)
         }
     }
 }
